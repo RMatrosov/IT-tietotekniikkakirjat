033d43820a2bfa9a52444276e7cad7f1cf067c16
Roman Matrosov <69420192+RMatrosov@users.noreply.github.com>
2021-09-14 14:37:02 +0300

    cart refactor

--- src/redux/action/books.ts
+++ src/redux/action/books.ts
@@ -7,7 +7,7 @@ import {ActionType, itemsType, setBooksActionType} from "../../types/books";
 
 export const ROOT_API_URL = 'http://localhost:3001/'
 export const HEROKU_API_URL = 'https://tietotekniikkakirjat.herokuapp.com'
-export const url = new URL(`${ROOT_API_URL}books?`)
+export const url = new URL(`${HEROKU_API_URL}books?`)
 
 export const setLoaded = (payload: boolean) => ({
     type: SET_LOADED,
